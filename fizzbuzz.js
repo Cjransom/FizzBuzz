8f2469067eb69c736ae351e29135722fc30de567
cjransom <cjohnransom@gmail.com>
2016-09-19 14:19:43 -0400

    First FizzBuzz Project

---
+++ fizzbuzz.js
@@ -0,0 +1,30 @@
+
+function fizzBuzz() {
+
+for (var i = 1; i <= 100; i++) {
+	if (i % 3 == 0 && i % 5 == 0) {
+		var node = document.createElement("LI");                
+		var textnode = document.createTextNode("FizzBuzz");       
+		node.appendChild(textnode);                             
+		document.getElementById("addme").appendChild(node);
+	}
+	else if (i % 5 == 0){
+		var node = document.createElement("LI");                
+		var textnode = document.createTextNode("Buzz");       
+		node.appendChild(textnode);                             
+		document.getElementById("addme").appendChild(node);
+	}
+	else if (i % 3 == 0) {
+		var node = document.createElement("LI");                
+		var textnode = document.createTextNode("Fizz");       
+		node.appendChild(textnode);                             
+		document.getElementById("addme").appendChild(node);
+	}
+	else {
+		var node = document.createElement("LI");                
+		var textnode = document.createTextNode(i);       
+		node.appendChild(textnode);                             
+		document.getElementById("addme").appendChild(node);
+	}
+}
+}
